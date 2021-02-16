319927c0ea4ce7c74ca92945273fa90c6b1a92ef
charbel_eid <charbel@better-stay.com>
2021-02-16 19:57:32 +0200

    commit 3 (seeder)

--- database/migrations/2021_02_16_161307_create_positions_table.php
+++ database/migrations/2021_02_16_161307_create_positions_table.php
@@ -15,6 +15,7 @@ class CreatePositionsTable extends Migration
     {
         Schema::create('positions', function (Blueprint $table) {
             $table->increments('IdPosition');
+            $table->timestamps();
             $table->string('Name');
             $table->boolean('Available');
         });
